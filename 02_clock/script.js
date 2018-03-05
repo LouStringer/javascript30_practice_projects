67f47529777f2b35d5508ded4e04f92f89b1fa23
Lou Stringer <louise.stringer@gmail.com>
2018-03-05 22:07:51 +0000

    02 clock: HTML and CSS

---
+++ 02_clock/script.js
@@ -0,0 +1,10 @@
+// Get current time:
+var date = new Date();
+var hour = date.getHours();
+var min = date.getMinutes();
+var sec = date.getSeconds();
+
+// set initial hand position:
+const hourHand = document.querySelector(".hour");
+const minuteHand = document.querySelector(".minute");
+const secondHand = document.querySelector(".second");
