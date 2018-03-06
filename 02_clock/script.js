4f0ba390244d1867d991813d3d0a5a9b4f35d460
Lou Stringer <louise.stringer@gmail.com>
2018-03-06 08:48:28 +0000

    02 clock: make hour hand move based on minutes as well as hours

--- 02_clock/script.js
+++ 02_clock/script.js
@@ -10,7 +10,7 @@ var min = date.getMinutes();
 var sec = date.getSeconds();
 
 // set initial hand positions:
-let hourDeg = ((hour * 30) + 90) + "deg";
+let hourDeg = ((hour * 30) + (min * 0.5) + 90) + "deg";
 let minDeg = ((min * 6) + 90) + "deg";
 let secDeg = ((sec * 6) + 90) + "deg";
 hourHand.style.transform = `rotate(${hourDeg})`;
@@ -19,21 +19,21 @@ secHand.style.transform = `rotate(${secDeg})`;
 
 // function to move hands:
 function moveHands () {
-  sec++;
-  secDeg = ((sec * 6) + 90) + "deg";
-  secHand.style.transform = `rotate(${secDeg})`;
   if (sec > 60) {
       sec = sec - 60;
       min++;
       minDeg = ((min * 6) + 90) + "deg";
       minHand.style.transform = `rotate(${minDeg})`;
+      hourDeg = ((hour * 30) + (min * 0.5) + 90) + "deg";
+      hourHand.style.transform = `rotate(${hourDeg})`;
       if (min > 60) {
         min = min - 60;
         hour++;
-        hourDeg = ((hour * 30) + 90) + "deg";
-        hourHand.style.transform = `rotate(${hourDeg})`;
       };
   };
+  sec++;
+  secDeg = ((sec * 6) + 90) + "deg";
+  secHand.style.transform = `rotate(${secDeg})`;
 }
 
 setInterval(moveHands, 1000);
