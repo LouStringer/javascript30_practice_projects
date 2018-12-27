b6856b287693dd4a3e836be3c0ac93bf4ac15081
Lou Stringer <louise.stringer@gmail.com>
2018-12-27 17:44:02 +0000

    add function to fetch weather forecast for city and event listener on search results

--- 06_Ajax_request/script.js
+++ 06_Ajax_request/script.js
@@ -1,9 +1,28 @@
 const inputField = document.querySelector('input[name=searchTerm]');
 const searchResults = document.querySelector('.search-results');
-const locationList = [];
+// const locationList = [];
+// let locationData = {};
 
 // grab the text entered into search box
-const getSearchTerm = () => document.querySelector("input[name=searchTerm]").value;
+const getSearchTerm = () => document.querySelector('input[name=searchTerm]').value;
+
+// display search results in UI
+const displaySearchResults = (data) => {
+  if (data.length >=1) {
+    const htmlLocations = data.map(location => {
+        return `<li class='location' data-woeid=${location.woeid}>${location.title}</li>`
+    }).join('');
+    searchResults.innerHTML = htmlLocations;
+    // grab searchResults.children and turn into an array - use forEach to attach event listener to each
+    // can this be done when generated?
+    // select all by class an iterate through
+    document.querySelectorAll('.location').forEach(location => {
+      location.addEventListener('click', fetchWeather);
+    })
+  } else {
+    searchResults.innerHTML = `<li>no results found</li>`
+  }
+}
 
 // search for locations based on the search term
 const searchLocation = () => {
@@ -11,20 +30,23 @@ const searchLocation = () => {
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      locationList.length = 0;
-      locationList.push(...data)
+      displaySearchResults(data);
+      // locationData = data;
     })
 }
 
-// display search results
-const displaySearchResults = () => {
-  const htmlLocations = locationList.map(location => {
-      return `<li>${location.title}</li>`
-  }).join('');
-  searchResults.innerHTML = htmlLocations;
+// event listener to run search and display results when text entered in box
+inputField.addEventListener('input', () => {
+  inputField.value.length > 1 ? searchLocation() : searchResults.innerHTML = '';
+});
+
+// next stage: click on search result to return five day forecast for that city (pick out temps + icon of weather), make it look good
+
+// use location woeid to fetch weather forecast
+const fetchWeather = (event) => {
+    const woeid = event.currentTarget.getAttribute('data-woeid');
+    const url = `http://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${woeid}`;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => console.log(data))
 }
-
-
-
-// write:
-// event listener on inputfield to perform search on key up
