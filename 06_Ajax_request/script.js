b926496541823aeb82ddc1d38f2d007313504569
Lou Stringer <louise.stringer@gmail.com>
2018-12-27 18:48:50 +0000

    format and display daily weather forecasts for selected location

--- 06_Ajax_request/script.js
+++ 06_Ajax_request/script.js
@@ -1,5 +1,7 @@
 const inputField = document.querySelector('input[name=searchTerm]');
-const searchResults = document.querySelector('.search-results');
+const searchResults = document.querySelector('#search-results');
+const locationHeading = document.querySelector('#forecast-location');
+const dailyForecastDisplay = document.querySelector('#daily-forecast');
 // const locationList = [];
 // let locationData = {};
 
@@ -13,14 +15,11 @@ const displaySearchResults = (data) => {
         return `<li class='location' data-woeid=${location.woeid}>${location.title}</li>`
     }).join('');
     searchResults.innerHTML = htmlLocations;
-    // grab searchResults.children and turn into an array - use forEach to attach event listener to each
-    // can this be done when generated?
-    // select all by class an iterate through
     document.querySelectorAll('.location').forEach(location => {
       location.addEventListener('click', fetchWeather);
-    })
+    });
   } else {
-    searchResults.innerHTML = `<li>no results found</li>`
+    searchResults.innerHTML = `<li>no results found</li>`;
   }
 }
 
@@ -40,13 +39,44 @@ inputField.addEventListener('input', () => {
   inputField.value.length > 1 ? searchLocation() : searchResults.innerHTML = '';
 });
 
-// next stage: click on search result to return five day forecast for that city (pick out temps + icon of weather), make it look good
-
-// use location woeid to fetch weather forecast
+// fetch weather forecast using woeid of clicked on function
 const fetchWeather = (event) => {
     const woeid = event.currentTarget.getAttribute('data-woeid');
     const url = `http://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${woeid}`;
     fetch(url)
       .then(response => response.json())
-      .then(data => console.log(data))
+      .then(data => {console.log(data); displayForecast(data)})
+}
+
+// to include: day | min temp | max temp | weather + icon | wind speed
+const displayForecast = data => {
+  const formatForecastDetails = data.consolidated_weather.map(day => {
+    return {
+      dayNumber: data.consolidated_weather.indexOf(day) + 1,
+      weather: day.weather_state_name.toLowerCase(),
+      minTemp: Math.round(day.min_temp),
+      maxTemp: Math.round(day.max_temp),
+      predictedTemp: Math.round(day.the_temp),
+      windSpeed: Math.round(day.wind_speed)
+    };
+  });
+  updateLocationHeading(data);
+  generateForecastHtml(formatForecastDetails);
+}
+
+const updateLocationHeading = (data) => {
+  locationHeading.innerHTML = `weather for ${data.title}`;
+}
+
+const generateForecastHtml = forecasts => {
+  const htmlForecasts = forecasts.map(day => {
+      return `<div class="day day${day.dayNumber}">
+                <p>day ${day.dayNumber}</p>
+                <p>weather conditions: ${day.weather}</p>
+                <p>temperature: ${day.predictedTemp}°c</p>
+                <p>range: ${day.minTemp}°c to ${day.maxTemp}°c</p>
+                <p>wind speed: ${day.windSpeed}</p>
+              </div>`
+  }).join('');
+  dailyForecastDisplay.innerHTML = htmlForecasts;
 }
